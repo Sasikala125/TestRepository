53c5882ae6a7d55e15af0eba62d58d8d13afe74e
Sasikala125 <52519649+Sasikala125@users.noreply.github.com>
2019-07-04 15:15:12 +0530

    Add files via upload

---
+++ User.js
@@ -0,0 +1,46 @@
+const bcryptService = require('../services/bcrypt.service');
+
+const tableName = 'users';
+const hooks = {
+    beforeCreate(user) {
+      user.password = bcryptService().password(user); // eslint-disable-line no-param-reassign
+    },
+  }
+
+module.exports = (sequelize, { STRING, INTEGER, BOOLEAN }) => {
+    const User = sequelize.define('User', {
+        id: {
+            type: INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        email: {
+            type: STRING,
+        },
+        password:{
+            type: STRING,
+        },
+        customer: {
+            type: BOOLEAN,
+        },
+        agent: {
+            type: BOOLEAN,
+        },
+        escrow: {
+            type: BOOLEAN,
+        },
+        admin: {
+            type: BOOLEAN,
+        },
+    }, { hooks,tableName });
+    
+    User.prototype.toJSON = function () {
+        const values = Object.assign({}, this.get());
+    
+        delete values.password;
+    
+        return values;
+      };
+
+    return User
+};
